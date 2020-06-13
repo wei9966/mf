d3971bc9025ecff87fafd61d15078dcaafb52508
wb_glb <weibin9966@qq.com>
2020-06-13 14:56:41 +0800

    2020.6.13

---
+++ SpringBootTest/src/main/java/com/wb/controller/QuickController.java
@@ -0,0 +1,14 @@
+package com.wb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuickController {
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+        return "你好 springboot";
+    }
+}
